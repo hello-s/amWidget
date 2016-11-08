a945f8f7d94ae62ab15ed1d191de2966bf701768
轩与 <wangwei.wang@alipay.com>
2016-11-08 10:45:45 +0800

    [dev] 修正小bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,24 @@
 var gulp = require('gulp'),
-	rename = require('gulp-rename'),
-	amTransportGulp = require('gulp-am-transport'),
-	uglify = require('gulp-uglify'),
-	amwDest = require('gulp-amw-dest');
+  rename = require('gulp-rename'),
+  amTransportGulp = require('gulp-am-transport'),
+  uglify = require('gulp-uglify'),
+  amwDest = require('gulp-amw-dest'),
+  concat = require('gulp-concat');
 
 gulp.task('build', function () {
-	gulp.src('./src/gallery/**/*.js')
-		.pipe(amTransportGulp({family: "AW", uglify: true}))
-		.pipe(amwDest('./dist/amw/'));
-
-	gulp.src('./src/gallery/**/*.js')
-		.pipe(amTransportGulp({family: "AW"}))
-		.pipe(gulp.dest('./examples/lib/gallery/'));
+  gulp.src('./src/gallery/**/*.js')
+    .pipe(amTransportGulp({family: "AW", uglify: true}))
+    .pipe(amwDest('./dist/amw/'));
+  
+  gulp.src('./src/gallery/**/*.js')
+    .pipe(amTransportGulp({family: "AW"}))
+    .pipe(gulp.dest('./examples/lib/gallery/'));
 });
 
 gulp.task('doc', function () {
-	gulp.src('./doc/*.md')
-		.pipe(concat('aw-doc.md'))
-		.pipe(gulp.dest('./doc/build'));
+  gulp.src('./doc/*.md')
+    .pipe(concat('aw-doc.md'))
+    .pipe(gulp.dest('./doc/build'));
 });
 
 gulp.task('default', ['build']);
